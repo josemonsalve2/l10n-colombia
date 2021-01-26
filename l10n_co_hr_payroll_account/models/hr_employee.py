00be3dbbe0f83d9e66a8bf31ff40f183eb9bd392
Alejandro Olano <alejocode@gmail.com>
2021-01-26 07:26:39 -0500

    [ADD] Addicional Support In Employee

--- l10n_co_hr_payroll_account/models/hr_employee.py
+++ l10n_co_hr_payroll_account/models/hr_employee.py
@@ -6,12 +6,12 @@ from odoo import models, fields
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    eps = fields.Many2many(comodel_name='res.partner',
-                           string='Health Pretender Entity',
-                           required=True)
-    pension = fields.Many2many(comodel_name='res.partner',
-                               string='Pension Pretender Entity',
-                               required=True)
-    layoff_fund = fields.Many2many(comodel_name='res.partner',
-                                   string='Pension Pretender Entity',
-                                   required=True)
+    eps = fields.Many2one(comodel_name='res.partner',
+                          string='Health Pretender Entity',
+                          required=True)
+    pension = fields.Many2one(comodel_name='res.partner',
+                              string='Pension Pretender Entity',
+                              required=True)
+    layoff_fund = fields.Many2one(comodel_name='res.partner',
+                                  string='Pension Pretender Entity',
+                                  required=True)
