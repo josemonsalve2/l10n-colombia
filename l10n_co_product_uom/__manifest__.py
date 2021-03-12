8f4e347d267e1f8829333a91a0ca389d9e025599
Alejandro Olano <alejocode@gmail.com>
2021-03-12 07:52:55 -0500

    [ADD] l10n_co Product UOM

---
+++ l10n_co_product_uom/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name":
+    "Colombian Unit of Measure Codes",
+    "version":
+    "11.0.1.0.0",
+    "website":
+    "https://github.com/exaap/l10n-colombia",
+    "author":
+    "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@JoanMarin, "
+    "Alejandro Olano Github@alejo-code",
+    "category":
+    "Localization",
+    "summary":
+    "Colombian Unit of Measure Codes",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/product.uom.code.csv",
+        "views/product_uom_views.xml",
+    ],
+    "installable":
+    True,
+}
