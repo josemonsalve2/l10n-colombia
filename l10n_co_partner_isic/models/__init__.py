87a7b5ecc673b494cc13bb71063eacf82885492d
Alejandro Olano <alejocode@gmail.com>
2021-03-15 14:13:58 -0500

    [ADD] l10n_co Partner ISIC

---
+++ l10n_co_partner_isic/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Joan Marín <Github@JoanMarin>
+# Copyright 2018 Guillermo Montoya <Github@guillermm>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import res_partner
+from . import res_partner_isic
