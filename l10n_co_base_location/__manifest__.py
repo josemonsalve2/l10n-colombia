3683945048a1f868abc0df0660253bdb483ccd4b
Alejandro Olano <alejocode@gmail.com>
2021-03-18 15:13:44 -0500

    [ADD] Location management (aka Better ZIP) - Colombian Data

---
+++ l10n_co_base_location/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Joan Marín <Github@JoanMarin>
+# Copyright 2018 Guillermo Montoya <Github@guillermm>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name":
+    "Location management (aka Better ZIP) - Colombian Data",
+    "summary":
+    "Colombian Data ZIP/Cities, States and Countries",
+    "version":
+    "12.0.1.0.0",
+    "license":
+    "AGPL-3",
+    "website":
+    "https://github.com/exaap/l10n-colombia",
+    "author":
+    "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@JoanMarin, "
+    "Guillermo Montoya Github@guillermm, "
+    "Alejandro Olano Github@alejo-code",
+    "category":
+    "Localization",
+    "depends": ["base_location"],
+    "data": [
+        "views/res_country_views.xml", "views/res_country_state_views.xml",
+        "views/res_city_zip_views.xml", "views/res_partner_views.xml"
+    ],
+    "installable":
+    True,
+}
