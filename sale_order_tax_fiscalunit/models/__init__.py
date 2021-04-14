436814bf53bfd2aa10e08fc14609820a63194ccc
Alejandro Olano <alejocode@gmail.com>
2021-04-14 08:10:52 -0500

    [ADD] Account Tax on Sale Order

---
+++ sale_order_tax_fiscalunit/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import sale_order
+from . import sale_order_line
