f6f8d0acb07daa25d7c63838918f55487a64dca1
Alejandro Olano <alejocode@gmail.com>
2021-05-03 10:30:27 -0500

    [FIX] Formas y medios de pago para la localizacion Colombiana

--- l10n_co_account_invoice_payment_mean/models/__init__.py
+++ l10n_co_account_invoice_payment_mean/models/__init__.py
@@ -3,6 +3,7 @@
 # Copyright 2019 Joan Marín <Github@JoanMarin>
 # Copyright 2021 Alejandro Olano <Github@alejo-code>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from . import account_payment_mean
 from . import account_payment_mean_code
 from . import account_invoice
