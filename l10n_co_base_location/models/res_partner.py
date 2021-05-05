99850fbebc17b8ef76a42b8a0e6c76825638f5d3
Alejandro Olano <alejocode@gmail.com>
2021-05-05 10:04:36 -0500

    [I18N] Update translation terms from Transifex

--- l10n_co_base_location/models/res_partner.py
+++ l10n_co_base_location/models/res_partner.py
@@ -3,6 +3,7 @@
 # Copyright 2018 Guillermo Montoya <Github@guillermm>
 # Copyright 2021 Alejandro Olano <Github@alejo-code>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo import models, fields
 
 
