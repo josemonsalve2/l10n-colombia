a60c98743913eda51c331548bb389246bf5be7a5
Alejandro Olano <alejocode@gmail.com>
2021-05-24 17:09:34 -0500

    [FIX] Colombian Account E-Invoicing

--- l10n_co_account_e_invoicing/__manifest__.py
+++ l10n_co_account_e_invoicing/__manifest__.py
@@ -8,7 +8,7 @@
     "category":
     "E-Invoicing",
     "version":
-    "12.0.1.0.0",
+    "11.0.1.0.0",
     "author":
     "EXA Auto Parts Github@exaap, "
     "Alejandro Olano Github@alejo-code, "
