14ae2e039b2ad44233f2fec7429174665b4945d8
Alejandro Olano <alejocode@gmail.com>
2021-05-28 15:11:41 -0500

    [FIX] Colombian Unit of Measure Codes

--- l10n_co_uom/models/uom_code.py
+++ l10n_co_uom/models/uom_code.py
@@ -6,8 +6,8 @@
 from odoo import fields, models
 
 
-class ProductUomCode(models.Model):
-    _name = 'product.uom.code'
+class UomCode(models.Model):
+    _name = 'uom.code'
 
     name = fields.Char(string='Name')
     code = fields.Char(string='Code')
