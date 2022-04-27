2f747bb7d11a1b2cfc73f1910a3ff19a26716d2a
Laura Catalina Villa Silva <laurav@EXA21.exaco.local>
2022-04-27 15:09:21 -0500

    [Add] Anexo 1.1 documento soporte

--- l10n_co_base_location/__manifest__.py
+++ l10n_co_base_location/__manifest__.py
@@ -5,30 +5,27 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name":
-    "Location management (aka Better ZIP) - Colombian Data",
-    "summary":
-    "Colombian Data ZIP/Cities, States and Countries",
-    "version":
-    "12.0.1.0.0",
-    "license":
-    "AGPL-3",
-    "website":
-    "https://github.com/exaap/l10n-colombia",
-    "author":
-    "EXA Auto Parts Github@exaap, "
-    "Alejandro Olano Github@alejo-code, "
-    "Joan Marín Github@JoanMarin, "
-    "Guillermo Montoya Github@guillermm,",
-    "category":
-    "Localization",
+    "name": "Location management (aka Better ZIP) - Colombian Data",
+    "summary": "Colombian Data ZIP/Cities, States and Countries",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/exaap/l10n-colombia",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin, "
+              "Alejandro Olano Github@alejo-code, "
+              "Guillermo Montoya Github@guillermm,",
+    "category": "Localization",
     "depends": ["base_location"],
     "data": [
-        "data/res_country_data.xml", "data/res_country_state_data.xml",
-        "views/res_country_views.xml", "views/res_country_state_views.xml",
-        "views/res_city_views.xml", "views/res_city_zip_views.xml",
+        "data/res_country_data.xml",
+        "data/res_country_state_data.xml",
+        "data/res_city_data.xml",
+        "data/res_city_zip_data.xml",
+        "views/res_country_views.xml",
+        "views/res_country_state_views.xml",
+        "views/res_city_views.xml",
+        "views/res_city_zip_views.xml",
         "views/res_partner_views.xml"
     ],
-    "installable":
-    True,
+    "installable": True,
 }
