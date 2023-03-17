a64b86eec345dcc5182e16c148f30fd80dcfd0c1
Yaazkal <yaazkal@gmail.com>
2023-03-17 11:27:37 -0500

    [MIG] l10n_co_account_fiscal_year: Migration to 16.0

--- l10n_co_account_fiscal_year/__manifest__.py
+++ l10n_co_account_fiscal_year/__manifest__.py
@@ -2,9 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Fiscal Year",
-    "version": "12.0",
-    "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    "version": "16.0.1.0.0",
+    "author": "EXA Auto Parts Github@exaap, "
+    "Alejandro Olano Github@alejo-code, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-colombia",
     "category": "Accounting & Finance",
     "depends": ["account_fiscal_year"],
     "data": ["views/account_fiscal_year_view.xml"],
+    "license": "AGPL-3",
 }
