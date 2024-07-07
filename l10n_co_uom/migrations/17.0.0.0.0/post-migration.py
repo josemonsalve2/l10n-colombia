8f4f13414f07e11675e1260ef59f6ebd8167eeb5
josemonsalve2 <josemonsalve2@gmail.com>
2024-07-06 20:28:41 -0700

    Migrating l10n_co_uom to 17.0.
    Updated the codes based on DIAN 1.9

---
+++ l10n_co_uom/migrations/17.0.0.0.0/post-migration.py
@@ -0,0 +1,8 @@
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    pass
---
+++ l10n_co_uom/migrations/17.0.0.0.0/pre-migration.py
@@ -0,0 +1,9 @@
+# Copyright 2018-2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    pass
