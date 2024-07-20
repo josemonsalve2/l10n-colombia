20a0f3a2cf921c983efa11e84329b90a243f099b
josemonsalve2 <josemonsalve2@gmail.com>
2024-07-20 14:57:38 -0700

    [IMP] account_tax_group_type: pre-commit auto fixes

--- account_tax_group_type/models/account_tax_group.py
+++ account_tax_group_type/models/account_tax_group.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Joan Marín <Github@JoanMarin>
 # Copyright 2021 Alejandro Olano <Github@alejo-code>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -9,5 +8,6 @@ from odoo import fields, models
 class AccountTaxGroup(models.Model):
     _inherit = "account.tax.group"
 
-    tax_group_type_id = fields.Many2one(string="Tax Group Type",
-                                        comodel_name="account.tax.group.type")
+    tax_group_type_id = fields.Many2one(
+        string="Tax Group Type", comodel_name="account.tax.group.type"
+    )
--- account_tax_group_type/models/account_tax_group_type.py
+++ account_tax_group_type/models/account_tax_group_type.py
@@ -1,22 +1,23 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Joan Marín <Github@JoanMarin>
 # Copyright 2021 Alejandro Olano <Github@alejo-code>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, _
+from odoo import _, fields, models
 
 
 class AccountTaxGroupType(models.Model):
-    _name = 'account.tax.group.type'
+    _name = "account.tax.group.type"
 
-    code = fields.Char(string='Code', required=True)
-    name = fields.Char(string='Name', required=True)
-    type = fields.Selection(selection=[('tax', 'Tax'),
-                                       ('withholding_tax', 'Withholding Tax')],
-                            string='Type',
-                            required=True,
-                            default=False)
-    description = fields.Char(string='Description')
+    code = fields.Char(string="Code", required=True)
+    name = fields.Char(string="Name", required=True)
+    type = fields.Selection(
+        selection=[("tax", "Tax"), ("withholding_tax", "Withholding Tax")],
+        string="Type",
+        required=True,
+        default=False,
+    )
+    description = fields.Char(string="Description")
 
-    _sql_constraints = [('code_uniq', 'unique (code)',
-                         _('The code of Tax Group Type must be unique!'))]
+    _sql_constraints = [
+        ("code_uniq", "unique (code)", _("The code of Tax Group Type must be unique!"))
+    ]
