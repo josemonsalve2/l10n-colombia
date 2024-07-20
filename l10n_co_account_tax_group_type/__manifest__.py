92fcb150e7db8cc924fe53a3c2f2aac938cfa77b
josemonsalve2 <josemonsalve2@gmail.com>
2024-07-20 12:14:18 -0700

    [IMP] l10n_co_account_tax_group_type: pre-commit auto fixes

--- l10n_co_account_tax_group_type/__manifest__.py
+++ l10n_co_account_tax_group_type/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Joan Marín <Github@JoanMarin>
 # Copyright 2021 Alejandro Olano <Github@alejo-code>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -8,7 +7,7 @@
     "summary": "Colombian Tax Group Types",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
-    "website": "https://github.com/exaap/l10n-colombia",
+    "website": "https://github.com/OCA/l10n-colombia",
     "author": "EXA Auto Parts Github@exaap, "
     "Alejandro Olano Github@alejo-code, "
     "Joan Marín Github@JoanMarin",
